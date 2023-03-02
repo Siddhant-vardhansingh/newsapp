ee0e7b526ddce42b28439afb633927ca44ec9da1
SIDDHANT VARDHAN SINGH <siddhant@SIDDHANTs-MacBook-Air.local>
2023-03-02 14:15:39 +0530

    seventh commit

--- src/App.js
+++ src/App.js
@@ -4,23 +4,26 @@ import React, { Component } from "react";
 import Navbar from "./components/Navbar";
 import News from "./components/News";
 
+
+
 export default class App extends Component {
   render() {
+    const apikey = "80a22bdfb6b54506848fe2360d814652"
     return (
       <div className="container">
         
         <BrowserRouter>
         <Navbar />
           <Routes>
-            <Route exact path="/" element={<News key="general" category="general"apikey="7b1f3a9137a84daca62c9e0e0d45f803"/> } />
-            <Route exact path="/business" element={<News key="business" category="business"apikey="7b1f3a9137a84daca62c9e0e0d45f803"/>} />
-            <Route exact path="/entertainment" element={<News key="entertainment" category="entertainment"apikey="7b1f3a9137a84daca62c9e0e0d45f803"/>} />
-            <Route exact path="/general" element={<News key="general" category="general"apikey="7b1f3a9137a84daca62c9e0e0d45f803"/>} />
-            <Route exact path="/health" element={<News key="health" category="health"apikey="7b1f3a9137a84daca62c9e0e0d45f803"/>} />
-            <Route exact path="/science" element={<News key="science" category="science"apikey="7b1f3a9137a84daca62c9e0e0d45f803"/>} />
-            <Route exact path="/sports" element={<News key="sports" category="sports"apikey="7b1f3a9137a84daca62c9e0e0d45f803"/>} />
-            <Route exactpath="/technology" element={<News key="technology" category="technology"apikey="7b1f3a9137a84daca62c9e0e0d45f803"/>} />
-            <Route exact path="/about" element={<News key="about" category="about"apikey="7b1f3a9137a84daca62c9e0e0d45f803"/>} />
+            <Route exact path="/" element={<News key="general" category="general"apikey={`${apikey}`}/> } />
+            <Route exact path="/business" element={<News key="business" category="business"apikey={`${apikey}`}/>} />
+            <Route exact path="/entertainment" element={<News key="entertainment" category="entertainment"apikey={`${apikey}`}/>} />
+            <Route exact path="/general" element={<News key="general" category="general"apikey={`${apikey}`}/>} />
+            <Route exact path="/health" element={<News key="health" category="health"apikey={`${apikey}`}/>} />
+            <Route exact path="/science" element={<News key="science" category="science"apikey={`${apikey}`}/>} />
+            <Route exact path="/sports" element={<News key="sports" category="sports"apikey={`${apikey}`}/>} />
+            <Route exactpath="/technology" element={<News key="technology" category="technology"apikey={`${apikey}`}/>} />
+            <Route exact path="/about" element={<News key="about" category="about"apikey={`${apikey}`}/>} />
           </Routes>
         </BrowserRouter>
       </div>
